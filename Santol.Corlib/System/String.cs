0107c486e7ef5fb485ceacfdcbd5f80cf367f572
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-04-21 23:24:31 +0100

    Add Santol.Corlib

---
+++ Santol.Corlib/System/String.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public sealed unsafe class String
+    {
+        public int Length { get; }
+        private char* _firstChar;
+    }
+}
