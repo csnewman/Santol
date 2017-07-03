f48f2c288cc357c87580aec538500d21ad8b2d02
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-03 19:37:07 +0100

    Add standard methods

---
+++ Santol/IR/IMethod.cs
@@ -0,0 +1,12 @@
+﻿namespace Santol.IR
+{
+    public interface IMethod
+    {
+        IType Parent { get; }
+        string Name { get; }
+        string MangledName { get; }
+        IType ReturnType { get; }
+        IType[] Arguments { get; }
+
+    }
+}
---
+++ Santol/IR/StandardMethod.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace Santol.IR
+{
+    public class StandardMethod : IMethod
+    {
+        public IType Parent { get; }
+        public string Name { get; }
+        public string MangledName { get; }
+        public IType ReturnType { get; }
+        public IType[] Arguments { get; }
+
+        public StandardMethod(IType parent, string name, IType returnType, IType[] arguments)
+        {
+            Parent = parent;
+            Name = name;
+            MangledName =
+                $"{parent.MangledName}_SM_{returnType.MangledName}_{name}_{string.Join("_", arguments.Select(f => f.MangledName))}";
+            ReturnType = returnType;
+            Arguments = arguments;
+        }
+    }
+}
