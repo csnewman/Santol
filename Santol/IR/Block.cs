4759cf4a701b3faa6d3ded092da08545a3c64dde
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-04 22:06:42 +0100

    Add empty block

--- Santol/IR/Block.cs
+++ Santol/IR/Block.cs
@@ -2,5 +2,13 @@
 {
     public class Block
     {
+        public string Name { get; }
+        public BlockRegion Region { get; }
+
+        public Block(string name, BlockRegion region)
+        {
+            Name = name;
+            Region = region;
+        }
     }
 }
