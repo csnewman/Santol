abdcb3ce98be4fa7ce5a0f11c9e64c1f4c51a7fb
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-18 00:02:20 +0100

    Fix platform allocate hook name

--- Santol/Hooks.cs
+++ Santol/Hooks.cs
@@ -7,7 +7,7 @@ namespace Santol
         public static readonly IMethod PlatformAllocate = new ExternalMethod()
         {
             Name = "Santol.Platform.PlatformHooks.Allocate",
-            MangledName = "C_Santol_Platform_PlatformHooks_SM_uintptr_uintptr",
+            MangledName = "C_Santol_Platform_PlatformHooks_SM_uintptr_Allocate_uintptr",
             IsStatic = true,
             Arguments = new IType[] {PrimitiveType.UIntPtr},
             ReturnType = PrimitiveType.UIntPtr
