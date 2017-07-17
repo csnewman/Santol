5ebd08fa84346205615552ac7a18185fc1b7fbcc
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-17 20:35:21 +0100

    Add platform allocate hook

---
+++ Santol/Hooks.cs
@@ -0,0 +1,16 @@
+﻿using Santol.IR;
+
+namespace Santol
+{
+    public static class Hooks
+    {
+        public static readonly IMethod PlatformAllocate = new ExternalMethod()
+        {
+            Name = "Santol.Platform.PlatformHooks.Allocate",
+            MangledName = "C_Santol_Platform_PlatformHooks_SM_uintptr_uintptr",
+            IsStatic = true,
+            Arguments = new IType[] {PrimitiveType.UIntPtr},
+            ReturnType = PrimitiveType.UIntPtr
+        };
+    }
+}
