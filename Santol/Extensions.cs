3099b896f2dd834f050ebeb178dcb3d5d111ea64
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-18 21:14:43 +0100

    Remove old extensions + Add signature matching extension

--- Santol/Extensions.cs
+++ Santol/Extensions.cs
@@ -1,39 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Mono.Cecil;
+using Santol.IR;
 
 namespace Santol
 {
     public static class Extensions
     {
-        public static string ToNiceString(this TypeReference type)
-        {
-            string extra = (type.IsValueType ? " val" : "") + (type.IsArray ? " array" : "") +
-                           (type.IsByReference ? " ref" : "") + (type.IsFunctionPointer ? " fncptr" : "") +
-                           (type.IsGenericInstance ? " geninst" : "") + (type.IsGenericParameter ? " genparam" : "") +
-                           (type.IsNested ? " nested" : "") + (type.IsOptionalModifier ? " opt" : "") +
-                           (type.IsPinned ? " pinned" : "") + (type.IsPointer ? " ptr" : "") +
-                           (type.IsPrimitive ? " prim" : "");
-            return $"{type.FullName}{(type.HasGenericParameters ? "<type>" : "")}({type.MetadataType}{extra})";
-        }
-
-        public static string GetName(this TypeReference definition)
-        {
-            return definition.FullName.Replace('.', '_').Replace("*", "PTR");
-        }
-
-        public static string GetName(this MethodReference definition)
-        {
-            return definition.DeclaringType.GetName() + "____" + definition.Name + "___" +
-                   string.Join("__", definition.Parameters.Select(p => p.ParameterType.GetName())) + "___" +
-                   definition.ReturnType.GetName();
-        }
-
-        public static string GetName(this FieldReference reference)
-        {
-            return reference.DeclaringType.GetName() + "____" + reference.Name + "___" + reference.FieldType.GetName();
-        }
-
         public static void AddOpt<T>(this IList<T> list, T value)
         {
             if (!list.Contains(value))
@@ -44,6 +15,18 @@ namespace Santol
         {
             foreach (T value in values)
                 list.AddOpt(value);
+        }
+
+        public static bool SignatureMatches(this IMethod method, IMethod otherMethod)
+        {
+            if (!method.ReturnType.Equals(otherMethod.ReturnType))
+                return false;
+            if (method.Arguments.Length != otherMethod.Arguments.Length)
+                return false;
+            for (int i = 0; i < method.Arguments.Length; i++)
+                if (!method.Arguments[i].Equals(otherMethod.Arguments[i]))
+                    return false;
+            return true;
         }
     }
 }
